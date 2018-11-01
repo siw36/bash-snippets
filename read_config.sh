f65e27e15eb246665e16572cb70938079a4ddc84
siw36 <rhh.klussmann@gmail.com>
2018-11-01 12:20:26 +0100

    converted to a function

--- read_config.sh
+++ read_config.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-declare -A VARS
-CONFIG='read_config_sample.ini'
+function READ_CONFIG () {
+	# $1=path to configuration file
 
-while IFS="=" read -r name val; do
-	VARS[$name]=$val
-done < $CONFIG
+	while IFS="=" read -r name val; do
+		VARS[$name]=$val
+	done < $1
+
+}
+
+# IMPORTANT: declare the associative array outside of the function!
+declare -A VARS
+
+READ_CONFIG 'read_config_sample.ini'
 
 # Access the parsed vars like this
 # ${VARS[VariableName]}
